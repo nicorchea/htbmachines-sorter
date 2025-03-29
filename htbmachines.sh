7179bf9cee2a1b040d36626f2f4e1ebecfc69484
nicorchea <derma@derma.local>
2025-03-29 16:44:00 -0300

    Agregado las funcionalidades necesarias para traer toda la informacion al sistema local, verificar y actualizar la base de datos en caso de ser necesario

--- htbmachines.sh
+++ htbmachines.sh
@@ -13,28 +13,67 @@ grayColour="\e[0;37m\033[1m"
 function ctrl_c(){
 
 echo -e "\n\n${redColour}[!] Saliendo... ${endColour}\n"
-exit 1
+tput cnorm && exit 1
 
 }
 
 #Ctrl_C
 trap ctrl_c INT
 
+#Variables Globales
+main_url="https://htbmachines.github.io/bundle.js"
+
 function helpPanel () {
-    echo -e "\n[+ This is the help panel:"]
+    echo -e "\n${yellowColour}[+]${endColour}${grayColour}Uso:${endColour}"
+    echo -e "\t${purpleColour}u)${endColour} ${grayColour}Actualizar lista de maquinas${endColour}"
+    echo -e "\t${purpleColour}m)${endColour} ${grayColour}Buscar por un nombre de maquina${endColour}"
+    echo -e "\t${purpleColour}h)${endColour} ${grayColour}Mostrar este panel de ayuda${endColour}"
 }
 
 function searchMachine() {
     machineName="$1";
-    echo $machineName;
+    echo "$machineName";
+}
+
+function updateFiles(){
+    
+    if [ ! -f bundle.js ]; then
+        echo -e "\n${yellowColour}[+]${endColour} Descargando archivos necesarios..."
+        curl -s $main_url > bundle.js
+        js-beautify bundle.js | sponge bundle.js
+        echo -e "\n${yellowColour}[+]${endColour} La descarga se a realizado con exito!"
+        tput cnorm
+
+    else
+        curl -s $main_url > bundle_temp.js
+        js-beautify bundle_temp.js | sponge bundle_temp.js
+        md5_temp_value=$(md5sum bundle_temp.js | awk '{print $1}')
+        md5_original_value=$(md5sum bundle.js | awk '{print $1}')
+
+    if [ "$md5_temp_value" == "$md5_original_value" ]; then
+        echo -e "\n${yellowColour}[+]${endColour} Comprobando si hay actualizaciones..."
+        echo -e "\n${yellowColour}[+]${endColour} Tu paquete esta actualizado :)"
+        rm bundle_temp.js
+    else
+        echo -e "${yellowColour}[+]${endColour} Comprobando si hay actualizaciones..."
+        sleep 3
+        echo -e "\n${yellowColour}[+]${endColour} Se han encontrado actualizaciones"
+        rm bundle.js && mv bundle_temp.js bundle.js
+        echo -e "\n${yellowColour}[+]${endColour} El paquete se ha actualizado exitosamente! :D"
+
+    fi
+
+        tput cnorm
+    fi
 }
 
 #indicators
 declare -i parameter_counter=0
 
-while getopts "m:h" arg; do
+while getopts "m:uh" arg; do
     case $arg in
-        m) machineName=$OPTARG; let parameter_counter+=1;;
+        m) machineName=$OPTARG; parameter_counter+=1;;
+        u) parameter_counter+=2;;
         h);;
         *) echo "Invalid Input"; helpPanel;;
     esac
@@ -42,6 +81,8 @@ done
 
 if [ $parameter_counter -eq 1 ]; then
     searchMachine $machineName
-else
+elif [ $parameter_counter -eq 2 ]; then
+    updateFiles
+else 
     helpPanel
-fi 
+fi
